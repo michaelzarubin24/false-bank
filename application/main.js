91325c700b42586881e214266b0e1ece1ebe6ee8
michaelzarubin24 <zarubinmihail99@gmail.com>
2023-07-30 11:10:30 +0300

    final version

--- application/main.js
+++ application/main.js
@@ -1,43 +1,89 @@
 "use strict";
+// Greeting
 alert("Welcome!");
+
+// Users storage
 const users = [];
+
+// Ask to write username
 const userName = getName();
 greeting(userName);
 
+// Main code
 let isRunning = true;
 while (isRunning) {
+  // Get user choice using a prompt
   const choice = getUserChoice();
-  switch (choice.toUpperCase) {
+
+  // Switch statement to handle different user choices
+  switch (choice.toUpperCase()) {
     case "A":
-      const userCard = parseInt(prompt("Enter your card number"));
-      const cardDate = parseInt(prompt("Card Date"));
-      const userCVV = parseInt(prompt("CVV"));
-      const userInfo = userData(userName, userCard, cardDate, userCVV);
-      users.push(userInfo);
+      // User wants to input new user data
+      const userCard = prompt("Enter your card number");
+      const cardDate = prompt("Card Date");
+      const userCVV = prompt("CVV");
+
+      // Validate the card number, date, and cvv before saving user data
+      if (
+        cardValidation(userCard) &&
+        cardValidation(cardDate, "date") &&
+        cardValidation(userCVV, "cvv")
+      ) {
+        const userInfo = userData(userName, userCard, cardDate, userCVV);
+        // Add the user data to the users array
+        users.push(userInfo);
+      } else {
+        // Show an error message if any input is not valid
+        showError("Invalid input! Please check your data.");
+      }
       break;
+
     case "B":
+      // Perform new scum operation (implementation can be customized)
+      newScumOperation();
       break;
+
     case "C":
+      // User wants to quit, so exit the while loop
       isRunning = false;
       break;
+
     default:
-      showError("Something went wrong , incorrect data ...");
+      // Show an error message for invalid user choices
+      showError("Incorrect data ...");
       break;
   }
 }
 
+// Greeting function to show a personalized welcome message
 function greeting(name) {
-  return "Hello, " + name + "!";
+  alert("Hello, " + name + "!");
 }
 
+// Get name from user function using a prompt
 function getName() {
   return prompt("Enter your name here!");
 }
 
+// Function to get user choice using a prompt
 function getUserChoice() {
   return prompt("a) Get users b) New scum operation c) Quit");
 }
 
+// Function to validate the card number, date, or cvv (checks if they have the specified number of digits)
+function cardValidation(input, type) {
+  const sanitizedInput = input.replace(/\D/g, "");
+  switch (type) {
+    case "date":
+      return sanitizedInput.length === 4;
+    case "cvv":
+      return sanitizedInput.length === 3;
+    default:
+      return sanitizedInput.length === 16;
+  }
+}
+
+// Function to create a user data object with provided information
 function userData(userName, userCard, cardDate, userCVV) {
   return {
     name: userName,
@@ -46,6 +92,16 @@ function userData(userName, userCard, cardDate, userCVV) {
     CVV: userCVV,
   };
 }
+
+// Function to perform a new scum operation (you can customize this function)
+function newScumOperation() {
+  alert("A new scum operation has been performed!");
+}
+
+// Function to show an error message using an alert
 function showError(msg) {
   alert(msg);
 }
+
+// Display the contents of the users array in the console
+console.log(["Users"], users);
